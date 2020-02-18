e49996e05f6bee6dd6eee45e9cc68a1302ceef5f
yanik <ext.yanik.nyang@gmail.com>
2020-02-18 22:08:33 +0100

    init project

---
+++ src/main/java/com/paperless/paperlessparent/PaperlessParentApplication.java
@@ -0,0 +1,13 @@
+package com.paperless.paperlessparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperlessParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperlessParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paperless/paperlessparent/PaperlessParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paperless.paperlessparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
